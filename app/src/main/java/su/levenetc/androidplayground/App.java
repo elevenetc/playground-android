9e5936785810b78269e3be5b65d40d76163956f9
Eugene Levenetc <shumvlesu@gmail.com>
2016-05-12 22:23:28 +0200

    Add App class

---
+++ app/src/main/java/su/levenetc/androidplayground/App.java
@@ -0,0 +1,12 @@
+package su.levenetc.androidplayground;
+
+import android.app.Application;
+
+/**
+ * Created by Eugene Levenetc on 12/05/2016.
+ */
+public class App extends Application {
+	@Override public void onCreate() {
+		super.onCreate();
+	}
+}
