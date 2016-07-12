2ec91c24703ef120c0d936e0e1fe30e9de911fce
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-12 23:58:43 +0200

    Update RotationViewPresenter

--- app/src/main/java/su/levenetc/androidplayground/activities/AlarmActivity.java
+++ app/src/main/java/su/levenetc/androidplayground/activities/AlarmActivity.java
@@ -5,14 +5,23 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import su.levenetc.androidplayground.presenter.RotationViewPresenter;
 import su.levenetc.androidplayground.services.AlarmStatusService;
+import su.levenetc.androidplayground.views.RotationSensorView;
 
 /**
  * Created by Eugene Levenetc on 10/07/2016.
  */
 public class AlarmActivity extends AppCompatActivity {
+
+	private RotationViewPresenter presenter;
+
 	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		RotationSensorView view = new RotationSensorView(this);
+		presenter = new RotationViewPresenter();
+		presenter.setView(view);
+		setContentView(view);
 		startService(new Intent(this, AlarmStatusService.class));
 	}
 }
--- app/src/main/java/su/levenetc/androidplayground/presenter/RotationViewPresenter.java
+++ app/src/main/java/su/levenetc/androidplayground/presenter/RotationViewPresenter.java
@@ -1,7 +1,15 @@
 package su.levenetc.androidplayground.presenter;
 
+import su.levenetc.androidplayground.views.RotationSensorView;
+
 /**
  * Created by Eugene Levenetc on 12/07/2016.
  */
 public class RotationViewPresenter {
+
+	private RotationSensorView view;
+
+	public void setView(RotationSensorView view) {
+		this.view = view;
+	}
 }
