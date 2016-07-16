0a72ac55cee61151525a993053208acf0c1f3868
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-16 20:13:57 +0200

    Set LogWriter disabled by default

--- app/src/main/java/su/levenetc/androidplayground/utils/LogWriter.java
+++ app/src/main/java/su/levenetc/androidplayground/utils/LogWriter.java
@@ -25,27 +25,34 @@ public class LogWriter {
 	private Context context;
 	private static final int TAG_LENGTH = 10;
 	private final DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss:SSS", Locale.ENGLISH);
+	private boolean enabled = false;
 
 	public LogWriter(String fileName, Context context) {
 		this.fileName = fileName;
 		this.context = context;
 	}
 
+	public void setEnable(boolean value) {
+		enabled = value;
+	}
+
 	public void log(String tag, String message) {
-		try {
-			tag = Utils.fixLength(tag, TAG_LENGTH, ' ');
-			String time = timeFormat.format(new Date(System.currentTimeMillis()));
+		if (enabled) {
+			try {
+				tag = Utils.fixLength(tag, TAG_LENGTH, ' ');
+				String time = timeFormat.format(new Date(System.currentTimeMillis()));
 
-			FileOutputStream out = context.openFileOutput(fileName, Context.MODE_APPEND);
-			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
-			String logMessage = time + " | " + tag + " | " + message + "\n";
-			outputStreamWriter.append(logMessage);
-			outputStreamWriter.close();
+				FileOutputStream out = context.openFileOutput(fileName, Context.MODE_APPEND);
+				OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
+				String logMessage = time + " | " + tag + " | " + message + "\n";
+				outputStreamWriter.append(logMessage);
+				outputStreamWriter.close();
 
-			Log.i(TAG, logMessage);
+				Log.i(TAG, logMessage);
 
-		} catch (IOException e) {
-			//ignore
+			} catch (IOException e) {
+				//ignore
+			}
 		}
 	}
 
@@ -62,7 +69,7 @@ public class LogWriter {
 				String receiveString;
 				StringBuilder sb = new StringBuilder();
 
-				while ((receiveString = bufferedReader.readLine()) != null){
+				while ((receiveString = bufferedReader.readLine()) != null) {
 					sb.append(receiveString);
 					sb.append("\n");
 				}
