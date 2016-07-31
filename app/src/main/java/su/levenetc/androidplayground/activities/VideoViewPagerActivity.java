e244cbd8f8082bd1d767b84e204284091306d037
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-31 16:40:17 +0200

    Add VideoViewPagerAdapter

--- app/src/main/java/su/levenetc/androidplayground/activities/VideoViewPagerActivity.java
+++ app/src/main/java/su/levenetc/androidplayground/activities/VideoViewPagerActivity.java
@@ -2,9 +2,13 @@ package su.levenetc.androidplayground.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
 
 import su.levenetc.androidplayground.R;
+import su.levenetc.androidplayground.adapters.VideoPagerAdapter;
+import su.levenetc.androidplayground.views.VideoViewPager;
 
 /**
  * Created by Eugene Levenetc on 31/07/2016.
@@ -13,5 +17,29 @@ public class VideoViewPagerActivity extends AppCompatActivity {
 	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_video_viewpager);
+
+		final VideoViewPager videoViewPager = (VideoViewPager) findViewById(R.id.video_viewpager);
+		final VideoPagerAdapter adapter = new VideoPagerAdapter(new int[]{
+				R.raw.video_01,
+				R.raw.video_02,
+				R.raw.video_03
+		});
+		videoViewPager.setAdapter(adapter);
+
+		videoViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			@Override public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+			}
+
+			@Override public void onPageSelected(int position) {
+				VideoView videoView = adapter.getItem(position);
+				videoView.seekTo(0);
+				videoView.start();
+			}
+
+			@Override public void onPageScrollStateChanged(int state) {
+
+			}
+		});
 	}
 }
---
+++ app/src/main/java/su/levenetc/androidplayground/adapters/VideoPagerAdapter.java
@@ -0,0 +1,54 @@
+package su.levenetc.androidplayground.adapters;
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.VideoView;
+
+/**
+ * Created by Eugene Levenetc on 31/07/2016.
+ */
+public class VideoPagerAdapter extends PagerAdapter {
+
+	private int[] rawFilesIds;
+	private VideoView[] items;
+
+	public VideoPagerAdapter(int[] rawFilesIds) {
+		this.rawFilesIds = rawFilesIds;
+		items = new VideoView[rawFilesIds.length];
+	}
+
+	@Override public Object instantiateItem(ViewGroup container, int position) {
+		VideoView videoView = new VideoView(container.getContext());
+		Uri uri = Uri.parse("android.resource://su.levenetc.androidplayground/raw/" + rawFilesIds[position]);
+		videoView.setBackgroundColor(position % 2 == 0 ? Color.RED : Color.BLUE);
+		videoView.setVideoURI(uri);
+		videoView.setZOrderOnTop(true);
+		//videoView.start();
+		container.addView(videoView);
+		items[position] = videoView;
+		return videoView;
+	}
+
+	@Override public void destroyItem(ViewGroup container, int position, Object object) {
+		container.removeView((View) object);
+		VideoView videoView = (VideoView) object;
+		videoView.stopPlayback();
+		videoView.suspend();
+		items[position] = null;
+	}
+
+	@Override public int getCount() {
+		return rawFilesIds.length;
+	}
+
+	@Override public boolean isViewFromObject(View view, Object object) {
+		return view == object;
+	}
+
+	public VideoView getItem(int position) {
+		return items[position];
+	}
+}
