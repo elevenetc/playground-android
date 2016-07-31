f2d94408fad19e5d736c7e9b40ba68b6d9dffe1b
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-31 16:40:36 +0200

    Fix height of VideoViewPager content

--- app/src/main/java/su/levenetc/androidplayground/views/VideoViewPager.java
+++ app/src/main/java/su/levenetc/androidplayground/views/VideoViewPager.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 
+import su.levenetc.androidplayground.utils.ViewUtils;
+
 /**
  * Created by Eugene Levenetc on 31/07/2016.
  */
@@ -14,5 +16,13 @@ public class VideoViewPager extends ViewPager {
 
 	public VideoViewPager(Context context, AttributeSet attrs) {
 		super(context, attrs);
+	}
+
+	@Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+		float screenWidth = ViewUtils.getScreenWidth(getContext());
+		float height = screenWidth / (16f / 9f);
+
+		super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec((int) height, MeasureSpec.EXACTLY));
 	}
 }
