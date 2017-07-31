c347bad5c2fceb02e9e4e0778f179fc11ffb646a
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-08-01 01:25:12 +0200

    QueryLine: update tree builder api sketch

--- app/src/test/java/su/levenetc/androidplayground/QueryLineTests.java
+++ app/src/test/java/su/levenetc/androidplayground/QueryLineTests.java
@@ -7,12 +7,10 @@ import su.levenetc.androidplayground.queryline.nodes.AutoCompleteNode;
 import su.levenetc.androidplayground.queryline.nodes.Node;
 import su.levenetc.androidplayground.queryline.nodes.NodesFactory;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static su.levenetc.androidplayground.QueryLineTests.AutoTree.*;
 
 public class QueryLineTests {
 
@@ -27,7 +25,6 @@ public class QueryLineTests {
 		queryModel.append('a');
 		queryModel.tryToComplete();
 
-
 		final LinkedList<Node> nodes = queryModel.getNodes();
 		assertEquals(3, nodes.size());
 		assertEquals("hello", nodes.get(0).toString());
@@ -36,11 +33,23 @@ public class QueryLineTests {
 	}
 
 	@Test
-	void list() {
-		new BuildNode().addSingleVariant(
-				new BuildNode("select-last"),
-				"select-a", "select-b"
-		);
+	public void list() {
+
+		final AutoTree tree =
+				or(
+						branch("select")
+								.then(array("name", "age"))
+								.thenAuto("where"),
+//								.then(array("users", "students"))
+//								.then(optional("limit", integers())),
+						branch("delete from")
+//								.then(array("users", "students"))
+//								.thenAuto("where")
+				);
+
+		if (tree == null) {
+
+		}
 	}
 
 	@NonNull private QueryModel getQueryModel() {
@@ -55,6 +64,78 @@ public class QueryLineTests {
 				return result;
 			}
 		});
+	}
+
+	static class AutoTree {
+
+		private List<String> variants;
+		private List<AutoTree> next;
+		private boolean optional;
+
+		public AutoTree(String... variants) {
+			this.variants = Arrays.asList(variants);
+		}
+
+		static AutoTree start() {
+			return new AutoTree();
+		}
+
+		static AutoTree or(AutoTree... branches) {
+			final AutoTree next = new AutoTree();
+			next.next = Arrays.asList(branches);
+			return next;
+		}
+
+		static AutoTree branch(String variant) {
+			return new AutoTree(variant);
+		}
+
+		static AutoTree variant(String variant) {
+			return new AutoTree();
+		}
+
+		AutoTree node(String variant) {
+			return new AutoTree();
+		}
+
+		AutoTree then(String... variant) {
+			return new AutoTree();
+		}
+
+		static AutoTree array(String... variant) {
+			return new AutoTree(variant);
+		}
+
+		static AutoTree optional(String optionalVariant, AutoTree variant) {
+			final AutoTree tree = new AutoTree(optionalVariant);
+			tree.optional = true;
+			tree.next = Arrays.asList(variant);
+			return variant;
+		}
+
+		static AutoTree integers(int... values) {
+			return new AutoTree();
+		}
+
+		AutoTree then(AutoTree variant) {
+			next = Arrays.asList(variant);
+			return this;
+		}
+
+		AutoTree thenAuto(String variant) {
+			next = Arrays.asList(new AutoTree(variant));
+			return this;
+		}
+
+		@Override public String toString() {
+
+			String[] lll = new String[variants.size()];
+			for (int i = 0; i < variants.size(); i++) {
+				lll[i] = variants.get(i);
+			}
+
+			return Arrays.toString(lll);
+		}
 	}
 
 	static class BuildNode {
