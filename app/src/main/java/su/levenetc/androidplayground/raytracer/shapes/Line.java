e6ac124b43b61f13fb170cb5f4f01739c42d3094
Eugene Levenetc <shumvlesu@gmail.com>
2018-05-06 20:32:09 +0200

    Raytracer: fix needless edge

--- app/src/main/java/su/levenetc/androidplayground/raytracer/shapes/Line.java
+++ app/src/main/java/su/levenetc/androidplayground/raytracer/shapes/Line.java
@@ -5,7 +5,7 @@ import su.levenetc.androidplayground.raytracer.Edge;
 public class Line extends Shape {
     public Line(double x1, double y1, double x2, double y2) {
         super(1);
-        Edge e = new Edge();
+        Edge e = edges.get(0);
         e.x1 = x1;
         e.y1 = y1;
         e.x2 = x2;
